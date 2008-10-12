8c8e5bdf8199117118118bacd031fc32d75fa810
Lars Olesen <lars@intraface.dk>
2008-10-12 17:43:17 +0000

    Coding standard improvements.
    git-svn-id: http://svn.intraface.dk/intrafacepublic/3Party/Ilib_Payment_Html_Provider_FakeQuickpay@1308 2fc64ab5-e821-0410-98e1-ee8f871d7b73

--- src/Ilib/Payment/Html/Provider/FakeQuickpay/Postprocess.php
+++ src/Ilib/Payment/Html/Provider/FakeQuickpay/Postprocess.php
@@ -1,30 +1,28 @@
 <?php
 /**
  * Fake class for testing postprocess Quickpay <www.quickpay.dk> online payments with html template
- * 
+ *
  * @author sune jensen <sj@sunet.dk>
  * @version 0.0.1
  * @package Payment_Html_Provider_FakeQuickpay
  * @category Payment
  * @license http://www.gnu.org/licenses/lgpl.html LGPL
  */
-
 require_once 'Ilib/Payment/Html/Provider/Quickpay/Postprocess.php';
 
 class Ilib_Payment_Html_Provider_FakeQuickpay_Postprocess extends Ilib_Payment_Html_Provider_Quickpay_Postprocess
 {
-    
     /**
      * Contructor
-     * 
+     *
      * @param string $merchant merchant number
      * @param string $language the language used in the payment
+     *
+     * @return void
      */
     public function __construct($merchant, $verification_key, $session_id)
-    {    
+    {
         parent::__construct($merchant, $verification_key, $session_id);
-    }    
+    }
 }
 
-
-?>
